5739c275953751825919c31cacc45b7ab70fbf0a
cedric <cedric.meleard@gmail.com>
2023-10-19 20:41:20 +0200

    Add object calisthenics dojo setup

---
+++ object-calisthenics-inventory-system/bookshop/bookshop.Domain.tests/Books/BookTests.cs
@@ -0,0 +1,10 @@
+namespace bookshop.Domain.tests.Books;
+
+public class BookTests
+{
+    [Fact]
+    public void Dummy()
+    {
+        Assert.True(true);
+    }
+}
---
+++ object-calisthenics-inventory-system/bookshop/bookshop.Domain/Books/Book.cs
@@ -0,0 +1,6 @@
+namespace bookshop.Domain.Books;
+
+public class Book
+{
+    
+}
